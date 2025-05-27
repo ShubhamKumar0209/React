fec945dd7f328bd19217c074146c300c31e70c03
Shubham Kumar <sshubhamkr0229@gmail.com>
2025-05-28 01:43:22 +0530

    Start working with the react environment

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+let name="My love";
+function App() {
+  return (
+    <>
+    <h2>Hello {name}</h2>
+      <div className="blank">Lovely</div>
+      
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="coatiner"><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium sint amet alias aperiam mollitia dolores odit culpa. Modi ipsam dolore excepturi consequatur delectus inventore error quos sint, quia tempore dolores, facilis, vel reprehenderit maxime!</p></div>
+      </>
+  );
+}
+
+export default App;

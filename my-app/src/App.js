19ae7cfc3d5f9d5dd6ffe93e2462d7374090090d
Shubham Kumar <sshubhamkr0229@gmail.com>
2025-05-29 01:23:43 +0530

    minor change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
 import './App.css';
+import About from './components/About';
 function App() {
   return (
     <>
     <Navbar title="TextUtils" aboutText="About us"/>
-    <div className="container my-3"><TextForm heading="Enter text to analyze"/></div>
+    <div className="container my-3">
+      <About/>
+      <TextForm heading="Enter text to analyze"/>
+      </div>
     </>
   );
 }
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 
 export default function TextForm(props) {
-    const [text,setText]=useState("Enter text here: ");  
+    const [text,setText]=useState("");  
     const handleUpClick=()=>{
          setText(text.toUpperCase());
     }
@@ -11,14 +11,34 @@ export default function TextForm(props) {
     const handleOnChange=(event)=>{
         setText(event.target.value);
     }
+    const handleClearClick=()=>{
+        setText("");
+    }
+    const handleReverseClick=()=>{
+        const reversedText=text.split(" ").map(word=>word.split("").reverse().join("")).join(" ");
+        setText(reversedText);
+    }
+    const words=text.trim()===""?0: text.trim().split(/\s+/).length
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" id="my-box" rows="8" value={text} onChange={handleOnChange}></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary" onClick={handleDownClick}>Convert to lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
+            <button className="btn btn-primary mx-2 my-2" onClick={handleReverseClick}>Reverse</button>
         </div>
+        <div className="container my-3">
+            <h1>Your text summary</h1>
+            <p>{words} words and {text.length} characters</p>
+            <p>Time to read {0.08*words} mins</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
+
     );
 }
